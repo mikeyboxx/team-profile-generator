ed936884c1b122278f36a6c37bdca05ff1123be8
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-22 20:54:56 -0400

    Added Engineer.js class

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
 const Intern = require('./lib/Intern.js');
+const Engineer = require('./lib/Engineer.js');
 
 const mike = new Employee('mike', '01', 'zzz_dsdds');
 const joe = new Employee('joe', '02', 'yyy_dsdds');
@@ -12,6 +13,7 @@ const bob = new Employee('bob', '04', 'fff__dsdds');
 
 const walter = new Manager('walter', '05', 'fff__dsdds', '67674');
 const intern1 = new Intern('intern1', '06', 'fff__dsddf77s', '99994');
+const engineer1 = new Engineer('Engineer1', '07', 'fxxxx_xxxxxs', 'mikeyboxx');
 
 
 console.log(
@@ -26,3 +28,9 @@ console.log(
     intern1.getEmail(),
     intern1.getRole(),
     intern1.getSchool());
+console.log(
+    engineer1.getName(),
+    engineer1.getId(),
+    engineer1.getEmail(),
+    engineer1.getRole(),
+    engineer1.getGitHub());
