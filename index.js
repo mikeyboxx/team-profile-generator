c66ff8591ab3cdc7cbcf5fc599bed86fb1c1bc71
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-22 20:39:06 -0400

    Added Manager.js class

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const Employee = require('./lib/Employee.js');
 
-const emp = new Employee('mike', '01', 'dsdds');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+
+const mike = new Employee('mike', '01', 'zzz_dsdds');
+const joe = new Employee('joe', '02', 'yyy_dsdds');
+const stella = new Employee('stella', 'www_03', 'dsdds');
+const bob = new Employee('bob', '04', 'fff__dsdds');
+
+const walter = new Manager('walter', '05', 'fff__dsdds', '67674');
+
+
 console.log(
-    emp.getName(),
-    emp.getId(),
-    emp.getEmail());
+    walter.getName(),
+    walter.getId(),
+    walter.getEmail(),
+    walter.getRole(),
+    walter.getOfficeNumber());
