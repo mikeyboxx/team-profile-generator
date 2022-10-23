260ddcf12aba152246f526c8916a8b44a4d1a1e4
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-23 12:29:29 -0400

    Changed the buildTeam method in Team.js to return a Promise

--- index.js
+++ index.js
@@ -1,56 +1,16 @@
 
 const chalk = require('chalk');
 const inquirer = require('inquirer');
-// const Manager = require('./lib/Manager.js');
-// const Intern = require('./lib/Intern.js');
-// const Engineer = require('./lib/Engineer.js');
 const Team = require('./lib/Team.js');
-// const promptInput = require('./src/promptInput.js');
-// const promptList = require('./src/promptList.js');
 
 
 const team = new Team();
 
-team.buildTeam();
+team.buildTeam()
+    .then(()=>{
+        console.log(team.toString());
+    });
 
-
-// (async function buildTeam(){
-//     const employees = [];
-
-//     const name =  await promptInput("Please Enter the Manager's Name:\n", true);
-//     const email =  await promptInput("Please Enter the Manager's Email:\n", true);
-//     const officeNumber = await promptInput("Please Enter the Manager's Office Number:\n", true);
-    
-//     employees.push(new Manager(name, email, officeNumber));
-//     console.log(`Manager added successfully!\nid = ${employees[0].getId()}\n\n`);
-    
-//     let choice = '';
-    
-//     while (choice !== 'Finish building my team'){
-//         choice = await promptList("Which team member would you like to add to the team?", ['Engineer', 'Intern', 'Finish building my team']);
-        
-//         if (choice==='Engineer'){
-//             const name =  await promptInput("Please Enter the Engineer's Name:\n", true);
-//             const email =  await promptInput("Please Enter the Engineer's Email:\n", true);
-//             const gitHub = await promptInput("Please Enter the Engineer's GitHub username:\n", true);
-//             employees.push(new Engineer(name, email, gitHub));
-//             console.log(`Engineer added successfully!\nid = ${employees[employees.length-1].getId()}\n\n`);
-//         }
-//         else if (choice==='Intern'){
-//             const name =  await promptInput("Please Enter the Intern's Name:\n", true);
-//             const email =  await promptInput("Please Enter the Intern's Email:\n", true);
-//             const school = await promptInput("Please Enter the Intern's School name:\n", true);
-//             employees.push(new Intern(name, email, school));
-//             console.log(`Intern added successfully!\nid = ${employees[employees.length-1].getId()}\n\n`);
-
-//         } else break;
-//     }
-
-//     console.log('Id' + '\t' + 'Name' + '\t' + 'Email' + '\t' + 'Role');
-//     console.log('-----------------------' + '\t' + '------------' + '\t' + '----------------' + '\t' + '---------');
-//     for(const employee of employees)
-//         console.log(employee.getId() + '\t' + employee.getName() + '\t' + employee.getEmail() + '\t' + employee.getRole());
-// });
 
 // A) Prompt for Manager Info (name, email)
 //    validate input - if not valid prompt again
