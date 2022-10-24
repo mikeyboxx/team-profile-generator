359a1ef5cb3ca9cde30ee8ffd0dc287ad2518f41
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-24 19:51:04 -0400

    Added tests for methods of the Employee.js class, in employee.test.js file

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -6,54 +6,81 @@ jest.mock("uuid");
 describe("Employee", () => {
     describe("Initialization", () => {
         it("should create an object with a name, email, and a generated id being private, and only accessed through getters", () => {
-        const name = "Mike";
-        const email = "happyanddebtfree@gmail.com";
+            const name = "Mike";
+            const email = "happyanddebtfree@gmail.com";
+            uuidv4.mockReturnValue('123456789');
 
-        uuidv4.mockReturnValue('123456789');
+            const employee = new Employee(name, email);
+            
+            expect(employee.getName()).toEqual(name);
+            expect(employee.getEmail()).toEqual(email);
+            expect(employee.getId()).toEqual('123456789');
 
-        const employee = new Employee(name, email);
-        
-        expect(employee.getName()).toEqual(name);
-        expect(employee.getEmail()).toEqual(email);
-        expect(employee.getId()).toEqual('123456789');
-
-        expect(employee.name).toEqual(undefined);
-        expect(employee.email).toEqual(undefined);
-        expect(employee.id).toEqual(undefined);
+            expect(employee.name).toEqual(undefined);
+            expect(employee.email).toEqual(undefined);
+            expect(employee.id).toEqual(undefined);
         });
-    })
+   
 
-    it("should throw an error, if provided no arguments", () => {
-        const cb = () => new Employee();
-       
-        expect(cb).toThrow();
+        it("should throw an error, if provided no arguments", () => {
+            const cb = () => new Employee();
+        
+            expect(cb).toThrow();
+        });
+    
+        it("should throw an error if 'name' is not a string", () => {
+            const cb = () => new Employee(2, "happyanddebtfree@gmail.com");
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });  
+
+        it("should throw an error if 'name' is an empty string", () => {
+            const cb = () => new Employee("", "happyanddebtfree@gmail.com");
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });  
+
+        it("should throw an error if 'email' is not a string", () => {
+            const cb = () => new Employee("Mike", 2);
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });  
+
+        it("should throw an error if 'email' is an empty string", () => {
+            const cb = () => new Employee("Mike", "");
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });  
     });
-  
-    it("should throw an error if 'name' is not a string", () => {
-        const cb = () => new Employee(2, "happyanddebtfree@gmail.com");
-        const err = new Error("Expected parameter 'name' to be a non-empty string");
 
-        expect(cb).toThrowError(err);
-    });  
+    describe("getRole()", () => {
+        it("should always return type 'Employee'", () => {
+            const name = "Mike";
+            const email = "happyanddebtfree@gmail.com";
+            uuidv4.mockReturnValue('123456789');
 
-    it("should throw an error if 'name' is an empty string", () => {
-        const cb = () => new Employee("", "happyanddebtfree@gmail.com");
-        const err = new Error("Expected parameter 'name' to be a non-empty string");
+            const employee = new Employee(name, email);
 
-        expect(cb).toThrowError(err);
-    });  
+            expect(employee.getRole()).toEqual('Employee');
+        });
+    });
 
-    it("should throw an error if 'email' is not a string", () => {
-        const cb = () => new Employee("Mike", 2);
-        const err = new Error("Expected parameter 'email' to be a non-empty string");
+    describe("toString()", () => {
+        it("should always return a formatted string of all properties and their values", () => {
+            const name = "Mike";
+            const email = "happyanddebtfree@gmail.com";
+            uuidv4.mockReturnValue('123456789');
 
-        expect(cb).toThrowError(err);
-    });  
+            const employee = new Employee(name, email);
 
-    it("should throw an error if 'email' is an empty string", () => {
-        const cb = () => new Employee("Mike", "");
-        const err = new Error("Expected parameter 'email' to be a non-empty string");
-
-        expect(cb).toThrowError(err);
-    });  
+            expect(employee.toString())
+                .toEqual(`id:   \t\t${employee.getId()}\n` +
+                        `name: \t\t${employee.getName()}\n` +
+                        `email:\t\t${employee.getEmail()}\n`);
+        });
+    });
 });
